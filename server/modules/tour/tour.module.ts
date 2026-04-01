a4c36eda3986cc63496a8e3ad00a2275e7b451e6
Seacare888 <79260248@qq.com>
2026-04-01 14:49:23 +0800

    Update tour.module.ts

--- server/modules/tour/tour.module.ts
+++ server/modules/tour/tour.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TourController } from './tour.controller';
 import { TourService } from './tour.service';
-@Module({ controllers: [TourController], providers: [TourService] })
+import { AppModule } from '../../app.module';
+
+@Module({
+  imports: [AppModule],
+  controllers: [TourController],
+  providers: [TourService],
+})
 export class TourModule {}
